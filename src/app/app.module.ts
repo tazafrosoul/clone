423d1cb5965827a5aefad8e50cf2391ee8700e31
Bless54 <blessrem19@gmail.com>
2022-03-30 23:54:41 +0300

    Included forms module to the inports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule, 
     HttpClientModule,
     LoadingBarHttpClientModule  
