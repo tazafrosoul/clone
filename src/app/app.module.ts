09552f37044d6617284737b5d4ea09813c619fe0
Salehe Mwitu <taz.afrosoul@gmail.com>
2022-04-17 15:24:08 +0300

    revamped Toolbar and segregated material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
+import { MaterialModule } from "./material/material.module";
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,12 +34,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     LoadingBarHttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule, MatButtonModule
+    MaterialModule
   ],
   providers: [NewsapiservicesService],
   bootstrap: [AppComponent],
   exports: [
-    MatToolbarModule
+    
   ]
 })
 export class AppModule { }
